357e36bfbcde0a7a000018af902e6d0cf962c371
Ashokmevada <ashokmevada18@gmail.com>
2023-11-01 16:45:04 +0530

    Callbacks added

---
+++ src/CNNClassifier/components/prepare_callbacks.py
@@ -0,0 +1,36 @@
+from CNNClassifier.entity.config_entity import PrepareCallbacksConfig
+import os
+import tensorflow as tf
+
+
+class PrepareCallback:
+    def __init__(self, config: PrepareCallbacksConfig):
+        self.config = config
+
+
+    @property
+    def create_tb_callbacks(self):
+        timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+        tb_running_log_dir = os.path.join(
+            self.config.tensorboard_dir , 
+            f"tb_logs_at_{timestamp}"
+        )
+
+        return tf.keras.callbacks.TensorBoard(log_dir = tb_running_log_dir)
+
+    @property
+    def create_ckpt_callbacks(self):
+        return tf.keras.callbacks.ModelCheckpoint(
+            filepath= str(self.config.checkpoint_model_filepath), # in this path is not allowed so for this we have converted windowPath to string
+            save_best_only = True
+        )
+
+
+    def get_tb_ckpt_callbacks(self):
+
+        return [
+            self.create_tb_callbacks,
+            self.create_ckpt_callbacks
+        ]
+
+
